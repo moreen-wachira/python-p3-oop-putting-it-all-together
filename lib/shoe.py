366c229dbd25303fbe550e0457c2175c21a208fd
Moreen <moreencharles@gmail.com>
2024-01-04 18:03:54 +0300

    Completed Assignment

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+        self.condition = 'New'
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+
+    def cobble_makes_new(self):
+        self.condition = 'New'
+        print("The shoe has been repaired, and its condition is now 'New'.")
